6e0a307ec353cb504ff092a6bc4582df1e31d8b3
ianngech <ianngech@gmail.com>
2022-04-01 14:27:44 +0300

    mergeAlternately added

--- src/AlternateStringMerge(1768).kt
+++ src/AlternateStringMerge(1768).kt
@@ -1,9 +1,26 @@
 fun main(){
-    mergeAlternately("abc", "pqr")
+    mergeAlternately("abcyuiweryuer", "pqr")
 }
 fun mergeAlternately(word1: String, word2: String): String {
     val charArray1 = word1.toCharArray()
     val charArray2 = word2.toCharArray()
+    val result = charArray1.zip(charArray2)
+    val charArray1Size = charArray1.size
+    val charArray2Size = charArray2.size
+    var resultString:String? = ""
+    result.forEach {pair ->
+        resultString+=pair.first
+        resultString+=pair.second
+    }
+    if(charArray1Size!=charArray2Size){
+        if(charArray1Size<charArray2Size) {
+            resultString += String((charArray2.slice(IntRange(charArray1Size, charArray2Size.minus(1)))).toCharArray())
+        }
+        else
+        {
+            resultString+= String(charArray1.slice(IntRange(charArray2Size,charArray1Size.minus(1))).toCharArray())
+        }
+    }
 
-    return ""
+    return resultString.toString()
 }
